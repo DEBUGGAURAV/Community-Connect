73f120f98efe3bacbbbf8f536db5efe668cda123
DEBUGGAURAV <jnplotus@gmail.com>
2026-09-23 20:21:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -372,7 +372,7 @@ app.post('/api/match/:requestId', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 async function startServer() {
   try {
